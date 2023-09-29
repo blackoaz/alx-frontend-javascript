4616ee1c00c04317e42d7afa338f05c74c670050
blackoaz <pauloakello56@gmail.com>
2023-09-29 17:49:29 +0300

    adding task 6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,15 +1,15 @@
 // 5-building.js
 
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-  }
+    constructor(sqft) {
+        this._sqft = sqft;
+    }
 
-  get sqft() {
-    return this._sqft;
-  }
+    get sqft() {
+        return this._sqft;
+    }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
 }
