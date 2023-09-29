cebfb2a663edbebd7b81917c68542f4553b89096
blackoaz <pauloakello56@gmail.com>
2023-09-29 16:37:54 +0300

    adding task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency{
+    constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string'){
+        throw new Error('Invalid type of variable')
+    }
+        this._code = code;
+        this._name = name;
+    }
+
+    get code() {
+        return this._code
+    }
+    set code (newCode) {
+        this._code = newCode
+
+    }
+
+    get name() {
+        return this._name
+    }
+    set code (newName) {
+        this._name = newName
+
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})` 
+    }
+}
