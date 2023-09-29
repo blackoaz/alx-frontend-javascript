160119c1457fdd6710a6812c19520b99d7866b4b
blackoaz <pauloakello56@gmail.com>
2023-09-29 11:34:16 +0300

    updates to task 7

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,12 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const userSignup = signUpUser(firstName, lastName);
-    const userPhoto = uploadPhoto(fileName);
-    return Promise.allSettled([userSignup, userPhoto])
-        .then((results) => {
-            return results.map(result => ({
-                status: result.status,
-                value: result.status === 'fulfilled' ? result.value : result.reason,
-            }));
-        });
+  const userSignup = signUpUser(firstName, lastName);
+  const userPhoto = uploadPhoto(fileName);
+  return Promise.allSettled([userSignup, userPhoto])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
