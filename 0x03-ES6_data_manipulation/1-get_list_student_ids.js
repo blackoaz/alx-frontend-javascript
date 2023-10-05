b1697f4966fb2b9a9518da63896d54e954c39042
blackoaz <pauloakello56@gmail.com>
2023-10-05 21:45:10 +0300

    adding task 2

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
-export default function getListStudentIds(students){
-    if (!Array.isArray(students)){
-        return []
-    }
-    return students.map(student => student.id)
-
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
 }
