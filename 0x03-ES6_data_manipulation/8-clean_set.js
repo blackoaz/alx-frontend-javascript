b1cd849c7e400a800140e8e41142a7cb08307051
blackoaz <pauloakello56@gmail.com>
2023-10-07 14:59:33 +0300

    adding update to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-    const resultArray = [];
-    set.forEach(value => {
-      if (startString === undefined || (startString !== '' && value.startsWith(startString))) {
-        resultArray.push(value.substring(startString.length));
-      }
-    });
-    return resultArray.join('-');
-  }
+  const resultArray = [];
+  set.forEach((value) => {
+    if (startString === undefined || (startString !== '' && value.startsWith(startString))) {
+      resultArray.push(value.substring(startString.length));
+    }
+  });
+  return resultArray.join('-');
+}
