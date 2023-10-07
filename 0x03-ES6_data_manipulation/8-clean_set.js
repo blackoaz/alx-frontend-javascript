4d321942c84672daf5388e7814469bc53646c1e1
blackoaz <pauloakello56@gmail.com>
2023-10-07 14:49:39 +0300

    adding task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,9 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set) || typeof startString !== 'string') {
     throw new Error('Invalid input types');
   }
+  if (startString === '') {
+    return '';
+  }
   const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
   const resultString = filteredValues.map((value) => value.slice(startString.length)).join('-');
 
